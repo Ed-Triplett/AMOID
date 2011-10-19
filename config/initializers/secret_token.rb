196919e22fe6de20ef385572888372d254726185
Ed Triplett <edward.triplett@gmail.com>
2011-10-19 13:45:22 -0400

    rails crap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amoid::Application.config.secret_token = '03d7a4a5f21c78e1bcb0fdde296b2dd41482da299d75fdbd0660aad3040ecb9f1e43d087cef7ddfa620c122a1df030c40655e7fb6bca0969a3bcf69101d73eca'
